ca624d81dcac19ce063c0d44fc0893a942b2ccb6
Gaurbhati9310-ui <gaurbhati9310@gmail.com>
2026-06-21 22:08:41 +0530

    Implement dailyTemperatures function using stack

---
+++ Week_3/Day_4/Daily_Temperatures.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        //optimal approach:
+
+        int n =temperatures.size();
+        stack<int>st;
+        vector<int>result(n);
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && temperatures[i]>=temperatures[st.top()]){
+                st.pop();
+            }
+            if(st.empty()){
+                result[i]= 0;
+            }
+            else{
+                result[i]=st.top()-i;
+            }
+            st.push(i);
+        }
+        return result;
+    }
+};
+
+// brute force:        
+//         vector<int>result(n);
+//      for(int i =0;i<n;i++){
+//         for(int j =i+1;j<n;j++){
+//             if(temperatures[j]>temperatures[i]){
+//                 result[i]=j-i;
+//                 break;
+//             }
+//         }
+//      }
+//     return result;
+//     }
+// };
