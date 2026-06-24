63d41ae57d28e8eb910de2f9f84a8f6f872b106e
Gaurbhati9310-ui <gaurbhati9310@gmail.com>
2026-06-24 13:39:34 +0530

    Update Sliding_Window_Maximum.cpp

--- Week_3/Day_5/Sliding_Window_Maximum.cpp
+++ Week_3/Day_5/Sliding_Window_Maximum.cpp
@@ -1,1 +1,26 @@
-
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int>dq;
+        vector<int>ans;
+        for(int i=0;i<k;i++){
+            while(!dq.empty() && nums[i] > dq.back()){
+                dq.pop_back();
+            }
+            dq.push_back(nums[i]);
+        }
+        ans.push_back(dq.front());
+        for(int i=k;i<nums.size() ;i++){
+            if(dq.front() == nums[i-k]){
+                dq.pop_front();
+            }
+        while(!dq.empty() && nums[i] > dq.back()){
+                dq.pop_back();
+            }
+            dq.push_back(nums[i]);
+            ans.push_back(dq.front());
+ 
+        }
+        return ans;
+    }
+};
