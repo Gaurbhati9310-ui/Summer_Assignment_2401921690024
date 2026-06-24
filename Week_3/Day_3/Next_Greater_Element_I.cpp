35308de342254d029398d469a0250d181c1263e5
Gaurbhati9310-ui <gaurbhati9310@gmail.com>
2026-06-24 11:27:40 +0530

    Update Next_Greater_Element_I.cpp

--- Week_3/Day_3/Next_Greater_Element_I.cpp
+++ Week_3/Day_3/Next_Greater_Element_I.cpp
@@ -1,1 +1,48 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        // approach 2:
+        // monotonic stack 
+        stack<int>st;
+        unordered_map<int,int>mp;
+        for(int i=nums2.size()-1;i>=0;i--){
+            while(!st.empty() && st.top() < nums2[i]){
+              st.pop();
+            }
+            mp[nums2[i]] = st.empty() ? -1 : st.top();
+            st.push(nums2[i]);
+        }
+        vector<int>ans;
+        for( int x : nums1){
+            ans.push_back(mp[x]);
+        }
+        return ans;
+       // approach 1:
+        // vector<int> ans;
 
+        // for(int i = 0; i < nums1.size(); i++) {
+
+        //     int pos = -1;
+
+        //     for(int j = 0; j < nums2.size(); j++) {
+        //         if(nums2[j] == nums1[i]) {
+        //             pos = j;
+        //             break;
+        //         }
+        //     }
+
+        //     int nge = -1;
+
+        //     for(int j = pos + 1; j < nums2.size(); j++) {
+        //         if(nums2[j] > nums1[i]) {
+        //             nge = nums2[j];
+        //             break;
+        //         }
+        //     }
+
+        //     ans.push_back(nge);
+        // }
+
+        // return ans;
+    }
+};
