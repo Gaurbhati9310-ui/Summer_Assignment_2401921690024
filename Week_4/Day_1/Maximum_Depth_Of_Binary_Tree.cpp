45e11165ac3153b69d391dc183a32cae290aaa3b
Gaurbhati9310-ui <gaurbhati9310@gmail.com>
2026-06-25 12:58:50 +0530

    Update Maximum_Depth_Of_Binary_Tree.cpp

--- Week_4/Day_1/Maximum_Depth_Of_Binary_Tree.cpp
+++ Week_4/Day_1/Maximum_Depth_Of_Binary_Tree.cpp
@@ -1,1 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        // dfs
+        // if(root==NULL){
+        //     return 0;
+        // }
+        // return max(maxDepth(root->left) , maxDepth(root->right))+1;
 
+        // bfs
+        queue< TreeNode* > q;
+        if(root==NULL){
+            return 0;
+        }
+        q.push(root);
+        int ans=0;
+        while(!q.empty()){
+            int n = q.size();
+            ans++;
+            for(int i=0;i<n;i++){
+                TreeNode *node = q.front();
+                q.pop();
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+        }
+        return ans; 
+    }
+};
