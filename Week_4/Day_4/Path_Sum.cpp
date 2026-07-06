03d6bdaecbee86c90b989c824f5d69882b116755
Gaurbhati9310-ui <gaurbhati9310@gmail.com>
2026-07-06 19:04:34 +0530

    Add path sum solution for binary tree
    Implement a solution to check if a binary tree has a path that sums to a target value.

--- Week_4/Day_4/Path_Sum.cpp
+++ Week_4/Day_4/Path_Sum.cpp
@@ -1,1 +1,34 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public: 
+bool inorder(TreeNode* root,int sum , int &targetSum){
+    if(root==NULL){
+        return false;
+    }
+    sum =sum +root->val;
+    if(root->left==NULL && root->right==NULL){
+        if(targetSum==sum){
+            return true;
+        }
+        return false;
+    }
+    bool leftside = inorder(root->left , sum , targetSum);
+    bool rightside = inorder(root->right , sum , targetSum);
+    return leftside || rightside;
+}
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        int sum =0;
+        bool pathsum = inorder(root,sum,targetSum);
+        return pathsum;
+    }
+};
