25541aaa21c452a3637d2d8de7eb68fd4574718a
Gaurbhati9310-ui <gaurbhati9310@gmail.com>
2026-07-07 10:37:53 +0530

    Implement maximum path sum for binary tree

--- Week_4/Day_4/Binary_Tree_Maximum_Path_Sum.cpp
+++ Week_4/Day_4/Binary_Tree_Maximum_Path_Sum.cpp
@@ -1,1 +1,33 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   
+   int maxsum;
+   int solve(TreeNode* root){
+    if(root==NULL){
+        return 0;
+    }
+    int left = solve(root->left);
+    int right = solve(root->right);
+    int neche_hi_mil_gya_answer = left+right + root->val; // 1
+    int koi_ek_acha = max(left,right)+ root->val;
+    int root_hi_acha = root->val;
+     maxsum = max({maxsum , neche_hi_mil_gya_answer , koi_ek_acha , root_hi_acha} );
+    return max(koi_ek_acha,root_hi_acha);
+   }
+    int maxPathSum(TreeNode* root) {
+        maxsum = INT_MIN;
+        solve(root);
+        return maxsum;
+    }
+};
