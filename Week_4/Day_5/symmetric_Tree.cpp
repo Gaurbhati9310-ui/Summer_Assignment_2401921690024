8468b92d84c151074cc3d94644ddef6990c8d9fe
Gaurbhati9310-ui <gaurbhati9310@gmail.com>
2026-07-08 22:28:25 +0530

    Add symmetric tree check implementation
    Implemented a solution to check if a binary tree is symmetric.

--- Week_4/Day_5/symmetric_Tree.cpp
+++ Week_4/Day_5/symmetric_Tree.cpp
@@ -1,1 +1,32 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool check_symmetry(TreeNode* l, TreeNode* r){
+        if(l==NULL && r==NULL){
+            return true;
+        }
+        if(l==NULL || r==NULL){
+            return false;
+        }
+        if((l->val == r->val)&&
+        check_symmetry(l->left , r->right)&&
+        check_symmetry(l->right, r->left)){
+        return true;
+        }
+        return false;
+    }
+    bool isSymmetric(TreeNode* root) {
+        return check_symmetry(root->left,root->right);
+        
+    }
+};
