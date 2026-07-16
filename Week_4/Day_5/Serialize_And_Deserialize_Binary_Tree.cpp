080f43ce8efd8d6d4ab3dec7ac421e41da4cec5f
Gaurbhati9310-ui <gaurbhati9310@gmail.com>
2026-07-16 23:40:32 +0530

    Implement serialize and deserialize for binary tree

--- Week_4/Day_5/Serialize_And_Deserialize_Binary_Tree.cpp
+++ Week_4/Day_5/Serialize_And_Deserialize_Binary_Tree.cpp
@@ -1,1 +1,76 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        if (!root) return "";
+
+        string s;
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            if (node) {
+                s += to_string(node->val) + ",";
+                q.push(node->left);
+                q.push(node->right);
+            } else {
+                s += "#,";
+            }
+        }
+
+        return s;
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        if (data.empty()) return NULL;
+
+        stringstream ss(data);
+        string item;
+
+        getline(ss, item, ',');
+        TreeNode* root = new TreeNode(stoi(item));
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            // left child
+            getline(ss, item, ',');
+            if (item != "#") {
+                node->left = new TreeNode(stoi(item));
+                q.push(node->left);
+            }
+
+            // right child
+            getline(ss, item, ',');
+            if (item != "#") {
+                node->right = new TreeNode(stoi(item));
+                q.push(node->right);
+            }
+        }
+
+        return root;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser, deser;
+// TreeNode* ans = deser.deserialize(ser.serialize(root));
